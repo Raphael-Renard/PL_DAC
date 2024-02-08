b26c23f2c3858bbf15dcb31ab6e914fb55cdf924
Raphaël R <93404597+Raphael-Renard@users.noreply.github.com>
2024-02-08 21:42:46 +0100

    Update mts.py

--- Jeux/Ultimate-Tic-Tac-Toe/mts.py
+++ Jeux/Ultimate-Tic-Tac-Toe/mts.py
@@ -1,5 +1,6 @@
 import math
 import random
+import copy
 
 class Node:
     def __init__(self, state, parent=None):
@@ -20,7 +21,10 @@ class Node:
         selected_child = None
         max_ucb_value = -float('inf')
         for child in self.children:
-            ucb_value = (child.wins / child.visits) + exploration_factor * math.sqrt(math.log(self.visits) / child.visits)
+            if child.visits == 0:
+                ucb_value = float('inf')  # Set a high value for unvisited nodes
+            else:
+                ucb_value = (child.wins / child.visits) + exploration_factor * math.sqrt(math.log(self.visits) / child.visits)
             if ucb_value > max_ucb_value:
                 selected_child = child
                 max_ucb_value = ucb_value
@@ -58,7 +62,6 @@ class MonteCarloTreeSearch:
             state = state.make_move(move)
         return state.get_result()
 
-# Example of a game state representation
 class GameState:
     def __init__(self,boards):
         self.boards = boards
@@ -68,16 +71,17 @@ class GameState:
     def get_possible_moves(self):
         moves = []
         if self.last_move != None:
-            coup_x,coup_y = self.last_move
-            if len(self.boards[coup_x][coup_y])>1:
+            board_x = self.last_move[0]//3
+            board_y = self.last_move[1]//3
+            if type(self.boards[board_x][board_y])==list:
                 for i in range(3):
                     for j in range(3):
-                        if self.boards[coup_x][coup_y][i][j] == 0:
-                            moves.append((i+3*coup_x, j+3*coup_y))
+                        if self.boards[board_x][board_y][i][j] == 0:
+                            moves.append((i+3*board_x, j+3*board_y))
             else:
                 for x in range(3):
                     for y in range(3):
-                        if len(self.boards[x][y])>1:
+                        if type(self.boards[x][y])==list:
                             for i in range(3):
                                 for j in range(3):
                                     if self.boards[x][y][i][j] == 0:
@@ -85,7 +89,7 @@ class GameState:
         else:
             for x in range(3):
                     for y in range(3):
-                        if len(self.boards[x][y])>1:
+                        if type(self.boards[x][y])==list:
                             for i in range(3):
                                 for j in range(3):
                                     if self.boards[x][y][i][j] == 0:
@@ -93,15 +97,14 @@ class GameState:
         return moves
 
     def make_move(self, move):
-        print(move)
         i, j = move
-        new_state = GameState(self.boards)
+        new_state = GameState(copy.deepcopy(self.boards))
         new_state.boards[i//3][j//3][i%3][j%3] = self.player
 
-        if [self.player,self.player,self.player] in self.boards[i//3][j//3] or [row[0] for row in self.boards[i//3][j//3]]==[self.player,self.player,self.player] or [row[1] for row in self.boards[i//3][j//3]]==[self.player,self.player,self.player] or [row[2] for row in self.boards[i//3][j//3]]==[self.player,self.player,self.player] or (self.boards[i//3][j//3][0][0]==self.player and self.boards[i//3][j//3][1][1]==self.player and self.boards[i//3][j//3][2][2]==self.player) or (self.boards[i//3][j//3][2][0]==self.player and self.boards[i//3][j//3][1][1]==self.player and self.boards[i//3][j//3][0][2]==self.player):
-            self.boards[i//3][j//3] = self.player
-        elif not(0 in self.boards[i//3][j//3][0]) and not(0 in self.boards[i//3][j//3][1]) and not(0 in self.boards[i//3][j//3][2]):
-            self.boards[i//3][j//3] = 0
+        if [self.player,self.player,self.player] in new_state.boards[i//3][j//3] or [row[0] for row in new_state.boards[i//3][j//3]]==[self.player,self.player,self.player] or [row[1] for row in new_state.boards[i//3][j//3]]==[self.player,self.player,self.player] or [row[2] for row in new_state.boards[i//3][j//3]]==[self.player,self.player,self.player] or (new_state.boards[i//3][j//3][0][0]==self.player and new_state.boards[i//3][j//3][1][1]==self.player and new_state.boards[i//3][j//3][2][2]==self.player) or (new_state.boards[i//3][j//3][2][0]==self.player and new_state.boards[i//3][j//3][1][1]==self.player and new_state.boards[i//3][j//3][0][2]==self.player):
+            new_state.boards[i//3][j//3] = self.player
+        elif not(0 in new_state.boards[i//3][j//3][0]) and not(0 in new_state.boards[i//3][j//3][1]) and not(0 in new_state.boards[i//3][j//3][2]):
+            new_state.boards[i//3][j//3] = 0
 
         new_state.player = -self.player
         new_state.last_move = move
@@ -112,7 +115,7 @@ class GameState:
             return True
         elif [-1,-1,-1] in self.boards or [row[0] for row in self.boards]==[-1,-1,-1] or [row[1] for row in self.boards]==[-1,-1,-1] or [row[2] for row in self.boards]==[-1,-1,-1] or (self.boards[0][0]==-1 and self.boards[1][1]==-1 and self.boards[2][2]==-1) or (self.boards[2][0]==-1 and self.boards[1][1]==-1 and self.boards[0][2]==-1):
             return True
-        elif [[len(self.boards[i][j]) for j in range(3)] for i in range(3)] == [[1,1,1],[1,1,1],[1,1,1]]:
+        elif [[type(self.boards[i][j]) for j in range(3)] for i in range(3)] == [[int,int,int],[int,int,int],[int,int,int]]:
             return True
         else:
             return False
@@ -123,12 +126,11 @@ class GameState:
             return 1
         elif [-self.player,-self.player,-self.player] in self.boards or [row[0] for row in self.boards]==[-self.player,-self.player,-self.player] or [row[1] for row in self.boards]==[-self.player,-self.player,-self.player] or [row[2] for row in self.boards]==[-self.player,-self.player,-self.player] or (self.boards[0][0]==-self.player and self.boards[1][1]==-self.player and self.boards[2][2]==-self.player) or (self.boards[2][0]==-self.player and self.boards[1][1]==-self.player and self.boards[0][2]==-self.player):
             return -1
-        elif [[len(self.boards[i][j]) for j in range(3)] for i in range(3)] == [[1,1,1],[1,1,1],[1,1,1]]:
+        elif [[type(self.boards[i][j]) for j in range(3)] for i in range(3)] == [[int,int,int],[int,int,int],[int,int,int]]:
             return 0
 
 
-# Example usage
-
+# Exemple
 initial_state = GameState([[[[0 for _ in range(3)] for _ in range(3)] for _ in range(3)] for _ in range(3)])
 mcts = MonteCarloTreeSearch(initial_state)
 best_move = mcts.select_move()
